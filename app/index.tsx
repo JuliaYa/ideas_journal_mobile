6b3e3fa361f80b0aa81f499c55332756037717ab
JuliaYa <julia.yakutova@gmail.com>
2025-08-25 14:47:07 +0300

    Add React Native Paper and icons

--- app/index.tsx
+++ app/index.tsx
@@ -1,22 +1,46 @@
-import { Button } from "@react-navigation/elements";
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from 'react-native';
+import { Button } from 'react-native-paper';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    paddingHorizontal: 20,
+  },
+
+  indexHeader:{
+    fontSize: 24,
+    fontWeight: "bold",
+    marginTop: 30,
+  },
+  button: {
+    marginLeft: 20,
+    padding: 10,
+    marginBottom: 10,
+    cursor: "pointer"
+  },
+});
 
 export default function Index() {
   return (
     <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+      style={styles.container}
     >
 
-      <Text style={{ fontSize: 20, fontWeight: "bold" }}>
+      <Text style={styles.indexHeader}>
         What do you whant to do?
-      </Text>  
-      <Button>View the List</Button>
-      <Button>Add New Idea</Button>
+      </Text> 
+      <View style={[styles.container, styles.buttonContainer]}>
+        <Button icon="format-list-text" mode="contained" style={styles.button}>View the List</Button>
+        <Button icon="grease-pencil" mode="contained" style={styles.button}>Add New Idea</Button>
+      </View> 
       <Button>Go to Settings</Button>
     </View>
   );
 }
+
