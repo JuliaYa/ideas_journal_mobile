c06a704f67d92f7bca32c6168a2f35d1547598ea
JuliaYa <julia.yakutova@gmail.com>
2025-10-03 16:43:12 +0300

    Add Delete button for IdeaDetails screen.

--- app/IdeaDetails.tsx
+++ app/IdeaDetails.tsx
@@ -1,17 +1,32 @@
 // screens/IdeaDetailsScreen.tsx
-import { useLocalSearchParams } from 'expo-router';
+import { useLocalSearchParams, useRouter } from 'expo-router';
 import React, { useCallback, useEffect, useState } from 'react';
 import { Image, View } from 'react-native';
-import { ActivityIndicator, Snackbar, Text } from 'react-native-paper';
-import { getIdea, Idea } from './services/ideas';
+import { ActivityIndicator, Button, Snackbar, Text } from 'react-native-paper';
+import { getIdea, deleteIdea, Idea } from './services/ideas';
 
 
 export default function IdeaDetailsScreen() {
+  const router = useRouter();
   const [idea, setIdea] = useState<Idea>();
   const [loading, setLoading] = useState<boolean>(true);
   const [error, setError] = useState<string | null>(null);
 
   const { id } = useLocalSearchParams<{ id: string }>();
+
+  const deleteItem = async () => {
+    setLoading(true);
+    setError(null);
+    try {
+      if (!id) throw new Error('Missing idea id');
+      await deleteIdea(id);
+      router.push("/IdeasList");
+    } catch (err: any) {
+      setError(err.message || 'Failed to delete idea');
+    } finally {
+      setLoading(false);
+    }
+  };
 
   const loadList = useCallback(async () => {
     setLoading(true);
@@ -48,6 +63,7 @@ export default function IdeaDetailsScreen() {
         <Text variant="bodyLarge" style={{marginBottom: 20}}>{idea?.description}</Text>
         <Text variant="bodySmall">Created: {new Date(idea?.created_at || '').toLocaleDateString()}</Text>
         <Text variant="bodySmall">Updated: {new Date(idea?.updated_at || '').toLocaleDateString()}</Text>
+        <Button onPress={deleteItem} style={{alignSelf: 'flex-end'}}>Delete</Button>
       </View>
       <Snackbar visible={!!error} onDismiss={() => setError(null)} action={{ label: 'Retry', onPress: () => loadList() }}>
         {error}
