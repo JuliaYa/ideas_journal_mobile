7cdec9fd594db4450c0650a4d4f4a75ae3895340
JuliaYa <julia.yakutova@gmail.com>
2026-04-01 13:13:49 +0300

    Use my mac IP to access api. Mobile and local tests will work with same link.

--- app/services/api.ts
+++ app/services/api.ts
@@ -1,8 +1,7 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import axios from "axios";
 
-const API_URL = "http://127.0.0.1:8000/api/"; // for working on a web part
-// const API_URL = "https://rnwct-95-216-27-23.a.free.pinggy.link/api/"  // for mobile testing
+const API_URL = "http://192.168.1.176:8000/api/"  // for mobile testing
 
 const api = axios.create({ baseURL: API_URL });
 
